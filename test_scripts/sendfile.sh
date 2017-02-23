1d4829cd956bba335bb69ba846c0b59f61d5cf55
Cassiano Reinert Novais dos Santos <crns13@gmail.com>
2017-02-23 17:02:02 -0300

    Correct broken line

--- test_scripts/sendfile.sh
+++ test_scripts/sendfile.sh
@@ -32,7 +32,7 @@ else
 			echo "Error: File $file does not exist."
 			continue
 		fi
-		http -a indc:indc@indc -f POST http://10.0.2.2:8000/uploads/datafile@"$file"
+		http -a indc:indc@indc -f POST http://10.0.2.2:8000/uploads/ datafile@"$file"
 	done
 fi
 
